0cffd777eaa1bdf53786db69e61a9d47e9609242
sbudisav <sbudisav@gmail.com>
2018-08-28 19:39:41 -0500

    Adds initial controller, view, and route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+    def single_product_action
+    @product = Product.first
+    render 'single_product.json.jbuilder'
+  end
+
+  def many_products_action
+    @products = Product.all
+    render 'many_products.json.jbuilder'
+  end
 end
